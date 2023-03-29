e718bc9aae54195465f2c63795c81adef45e4760
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-29 18:09:17 +0100

    header done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  return <div className="text-left">Head</div>;
+  return (
+    <div className="font-raleway my-4 mx-6 md:mx-16 lg:mx-32">
+      <header className="font-raleway font-medium flex justify-end text-[#4F4F4F]">
+        <div className="flex items-center gap-2">
+          <button className="text-lg leading-5">random</button>
+          <span className="material-icons">autorenew</span>
+        </div>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
 }
 
 export default App;
